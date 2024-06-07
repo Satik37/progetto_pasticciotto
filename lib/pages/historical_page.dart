926df15049984ec31c2f9bb84f8e737304e56039
Satik37 <satmil@tiscali.it>
2024-06-07 12:56:54 +0200

    Updated logic and historical pages

--- lib/pages/historical_page.dart
+++ lib/pages/historical_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:sushi/components/image_widget.dart';
 import 'package:sushi/themes/colors.dart';
+import '../components/puzzles_tile.dart';
+import 'puzzles_detail_page.dart';
 
 class HistoricalPage extends StatefulWidget {
   const HistoricalPage({super.key});
@@ -12,107 +13,210 @@ class HistoricalPage extends StatefulWidget {
 
 class _HistoricalPageState extends State<HistoricalPage> {
   // List of image paths
-  final List<String> imagePaths = [
-    'images/archangelMichael.png',
-    'images/danzaDeLaMuerte10.png',
-    'images/danceOfDeath.png',
-    'images/danzaDeLaMuerte.png',
-    'images/danzaDeLaMuerte3.png',
-    'images/danzaDeLaMuerte4.png',
-    'images/danzaDeLaMuerte5.png',
-    'images/danzaDeLaMuerte6.png',
-    'images/danzaDeLaMuerte8.png',
+  final List<Map<String, String>> puzzles = [
+    //  1
+    {
+      'path': 'images/danzaDeLaMuerte10.png',
+      'name': 'The Disappearing General',
+      'description':
+          'During the Roman Empire, General Marcus was sent to the northern territories and never returned. Years later, his diary was found, ending abruptly with the words, "I have found the truth." What happened to General Marcus?',
+      'answer':
+          'General Marcus discovered a betrayal within his ranks. Before he could reveal the traitors, they captured and killed him, ensuring his silence.',
+    },
+    //  2
+    {
+      'path': 'images/danceOfDeath.png',
+      'name': 'The Pharaoh\'s Secret',
+      'description':
+          'An ancient Egyptian pharaoh was buried with a secret that was supposed to bring prosperity to the kingdom. However, the kingdom fell into ruin after his death. What was the secret?',
+      'answer':
+          'The pharaoh had hidden a map to a vast gold mine. When he died, the secret was lost, and the kingdom couldn\'t sustain its economy without the resource.',
+    },
+    //  3
+    {
+      'path': 'images/danzaDeLaMuerte8.png',
+      'name': 'The Vanished Viking',
+      'description':
+          'A Viking chief led an expedition to a new land but vanished without a trace. Centuries later, an artifact bearing his name is found in a distant desert. How did it get there?',
+      'answer':
+          'The chief was captured by traders from a distant land and sold into slavery. The artifact was a personal item he managed to keep.',
+    },
+    //  4
+    {
+      'path': 'images/danzaDeLaMuerte6.png',
+      'name': 'The Hidden Treasure',
+      'description':
+          'A pirate buried his treasure on an island and left a map with cryptic clues. Despite many attempts, it was never found. What was the key to the map?',
+      'answer':
+          'The key to the map was a specific astronomical event that only occurred every 50 years. Without this timing, the clues made no sense.',
+    },
+    //  5
+    {
+      'path': 'images/danzaDeLaMuerte5.png',
+      'name': 'The Ghost Ship',
+      'description':
+          'In the 17th century, a ship set sail with a precious cargo and vanished. It reappeared decades later, abandoned. What happened to the crew?',
+      'answer':
+          'The crew was stricken by an unknown disease. They abandoned the ship in search of help on a nearby island but succumbed, leaving the ship to drift.',
+    },
+    //  6
+    {
+      'path': 'images/danzaDeLaMuerte4.png',
+      'name': 'The Phantom Knight',
+      'description':
+          'During the Crusades, a knight known for his ruthless tactics disappeared. Years later, his armor was found buried beneath a church, untouched by time. The inscription read, "Condemned by the righteous." What happened to the knight?',
+      'answer':
+          'The knight was betrayed by his fellow crusaders who couldn\'t bear his brutality. They lured him into the church, where he was entombed alive.',
+    },
+    //  7
+    {
+      'path': 'images/danzaDeLaMuerte3.png',
+      'name': 'The Missing Pharaoh',
+      'description':
+          'An Egyptian pharaoh vanished, leaving behind a cryptic message: "The sands reveal all at dusk." His body was never found, and his tomb lay empty. What does the message mean?',
+      'answer':
+          'The pharaoh\'s body was buried in a secret chamber that could only be found when the setting sun cast specific shadows on the sands, revealing the entrance.',
+    },
+    //  8
+    {
+      'path': 'images/danzaDeLaMuerte.png',
+      'name': 'The Cursed Treasure',
+      'description':
+          'A pirate captain buried his treasure on a deserted island. His map, found years later, had the warning: "Greed leads to doom." Many sought the treasure but never returned. Why?',
+      'answer':
+          'The treasure was buried in a cave that flooded with the tide. Those who lingered too long, driven by greed, were trapped and drowned.',
+    },
+    //  9
+    {
+      'path': 'images/archangelMichael.png',
+      'name': 'Historical puzzle 9',
+      'description': 'This is the description of the puzzle 9',
+      'answer': 'answer to the puzzle 9',
+    },
+
     //'images/danzaDeLaMuerte09.png',
 
     // Add more image paths as needed
   ];
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        backgroundColor: const Color.fromARGB(255, 100, 100, 100),
+  Widget build(BuildContext context) => Container(
+        // ----- Background color
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.red,
+              // Another Color,
+              const Color.fromARGB(255, 50, 50, 50),
+            ],
+          ),
+        ),
+        // backgroundColor: const Color.fromARGB(255, 100, 100, 100),
 
-        // sliver app bar
+        // ----- Body
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            fit: StackFit.expand,
 
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: Colors.red,
-              expandedHeight: 200,
-              floating: true,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Image.asset(
-                  'images/knightHorse.png',
-                  fit: BoxFit.cover,
-                ),
-                title: Text(
-                  'Historical Fiction',
-                  style: GoogleFonts.dmSerifDisplay(
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    fontSize: 25,
-                  ),
-                ),
-                centerTitle: true,
-              ),
-              leading: IconButton(
-                icon: Icon(
-                  Icons.arrow_back,
-                  color: Colors.blue,
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ),
+            // ----- Add background image
+            children: [
+              //   Image.asset(
+              //     'images/skull18.png',
+              //     fit: BoxFit.cover,
+              //   ),
 
-            // end of bar
+              // ----- sliver app bar
 
-            // sliver list
-
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  // promo banner
-
-                  Container(
-                    decoration: BoxDecoration(
-                      color: primaryColor,
-                      borderRadius: BorderRadius.circular(20),
+              CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    backgroundColor: Colors.transparent,
+                    expandedHeight: 200,
+                    floating: true,
+                    pinned: true,
+                    flexibleSpace: FlexibleSpaceBar(
+                      background: Image.asset(
+                        'images/knightHorse.png',
+                        fit: BoxFit.cover,
+                      ),
+                      title: Text(
+                        'Historical Fiction',
+                        style: GoogleFonts.dmSerifDisplay(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 25,
+                        ),
+                      ),
+                      centerTitle: true,
                     ),
-                    margin: const EdgeInsets.only(
-                        left: 25, right: 25, bottom: 0, top: 20),
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              // promo message
+                    leading: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.blue,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
 
-                              Text(
-                                'Echoes of the fallen: history\'s darkest hours reimagined.',
-                                style: GoogleFonts.dmSerifDisplay(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red,
+                  // end of bar
+
+                  // sliver list
+
+                  SliverList(
+                    delegate: SliverChildListDelegate(
+                      [
+                        // promo banner
+
+                        Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(155, 50, 50, 50),
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: Colors.red,
+                              width: 2,
+                            ),
+                          ),
+                          margin: const EdgeInsets.only(
+                              left: 25, right: 25, bottom: 0, top: 20),
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    // promo message
+
+                                    Text(
+                                      'Echoes of the fallen:                     history\'s darkest hours reimagined.',
+                                      style: GoogleFonts.dmSerifDisplay(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 5),
+                                  ],
                                 ),
                               ),
-                              const SizedBox(height: 5),
+
+                              // add more widgets here
                             ],
                           ),
                         ),
-
-                        // add more widgets here
                       ],
                     ),
                   ),
+                  buildImages(),
                 ],
               ),
-            ),
-            buildImages(),
-          ],
+            ],
+          ),
         ),
       );
 
@@ -122,15 +226,31 @@ class _HistoricalPageState extends State<HistoricalPage> {
         child: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
           ),
           primary: false,
           shrinkWrap: true,
-          itemCount: imagePaths.length,
-          itemBuilder: (context, index) => ImageWidget(
-            imagePath: imagePaths[index],
-          ),
+          padding: const EdgeInsets.all(10),
+          itemCount: puzzles.length,
+          itemBuilder: (context, index) {
+            final puzzle = puzzles[index];
+            return PuzzlesTile(
+              imagePath: puzzle['path']!,
+              puzzleName: puzzle['name']!,
+              puzzleColor: Colors.red, // Customize the color as needed
+              onTap: () {
+                // Navigate to a new page for each puzzle
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PuzzleDetailPage(puzzle: puzzle),
+                  ),
+                );
+              },
+            );
+          },
         ),
       );
-
   // something else
 }
--- lib/pages/logic_page.dart
+++ lib/pages/logic_page.dart
@@ -14,57 +14,78 @@ class LogicPage extends StatefulWidget {
 class _LogicPageState extends State<LogicPage> {
   // List of image paths
   final List<Map<String, String>> puzzles = [
+    //  1
     {
       'path': 'images/danzaDeLaMuerte10.png',
-      'name': 'Puzzle uno 1',
-      'description': 'Descrizione puzzle 1',
-      'answer': 'Risposta completa per il puzzle 1',
+      'name': 'Poisoned Drink',
+      'description':
+          'Two friends order the same drink at a restaurant. One drinks it slowly and survives; the other drinks it quickly and dies. The drink was poisoned. Why did one survive?',
+      'answer':
+          'The poison was in the ice. The friend who drank slowly allowed the ice to melt, releasing the poison. The friend who drank quickly did not.',
     },
+    //  2
     {
       'path': 'images/danceOfDeath.png',
-      'name': 'Puzzle 2',
-      'description': 'This is the description of the puzzle two2',
-      'answer': 'answer to the puzzle two2',
+      'name': 'The Silent Witness',
+      'description':
+          'A famous painter was found dead in his locked studio. The only way in or out was through the door, and the key was found in his pocket. On the easel was an unfinished painting depicting a faceless man. The painter\'s apprentice, who had no alibi, claimed innocence. What clue reveals the murderer?',
+      'answer':
+          'The faceless man in the painting. The painter was trying to reveal the identity of his killer, who was someone he knew but couldn\'t explicitly name – the apprentice.',
     },
+    //  3
     {
       'path': 'images/danzaDeLaMuerte8.png',
-      'name': 'Puzzle 3',
-      'description': 'This is the description of the puzzle 3',
-      'answer': 'answer to the puzzle 3',
+      'name': 'The Vanishing Neighbor',
+      'description':
+          'A woman noticed her reclusive neighbor, Mr. Black, hadn\'t been seen for days. She entered his home and found a disturbing scene: a chair turned over, a broken vase, and a note that read, "It\'s behind you." Just then, she heard a creak. What did she discover?',
+      'answer':
+          'The woman realized the note referred to something in the room behind her. Turning around, she discovered a hidden passage leading to a basement where Mr. Black was held captive.',
     },
+    //  4
     {
       'path': 'images/danzaDeLaMuerte6.png',
-      'name': 'Puzzle 4',
-      'description': 'This is the description of the puzzle 4',
-      'answer': 'answer to the puzzle 4',
+      'name': 'The Cursed Mirror',
+      'description':
+          'A man bought an antique mirror rumored to show the true nature of one\'s soul. Every morning, he noticed the reflection showing something ominous: a shadowy figure behind him. Fearing for his life, he smashed the mirror, only to find a hidden compartment behind it. What was inside?',
+      'answer':
+          'Inside the compartment was a diary detailing a murder committed by the mirror\'s previous owner. The shadowy figure was the ghost of the victim, trying to reveal the truth.',
     },
+    //  5
     {
       'path': 'images/danzaDeLaMuerte5.png',
-      'name': 'Puzzle 5',
-      'description': 'This is the description of the puzzle 5',
-      'answer': 'answer to the puzzle 5',
+      'name': 'The Betrayed Soldier',
+      'description':
+          'During the war, a soldier received an anonymous letter warning of a traitor among his squad. The next day, he found his best friend dead with a note saying, "Trust no one." How can the soldier identify the traitor?',
+      'answer':
+          'The traitor is the person who wrote the anonymous letter. They sought to create distrust and eliminate the best friend, who likely suspected them.',
     },
+    //  6
     {
       'path': 'images/danzaDeLaMuerte4.png',
-      'name': 'Puzzle 6',
-      'description': 'This is the description of the puzzle 6',
-      'answer': 'answer to the puzzle 6',
+      'name': 'The Vanishing Lighthouse Keeper',
+      'description':
+          'The lighthouse keeper was last seen entering the lighthouse during a storm. When the storm passed, he was gone, leaving behind a lighthouse full of lit candles and a diary entry, "The light must never go out." Where did he go?',
+      'answer':
+          'The keeper discovered a hidden passage within the lighthouse that led to a secret cavern. The lit candles were his way of ensuring the light remained, hinting at his knowledge of the passage.',
     },
+    //  7
     {
       'path': 'images/danzaDeLaMuerte3.png',
-      'name': 'Puzzle 7',
+      'name': 'Logical puzzle 7',
       'description': 'This is the description of the puzzle 7',
       'answer': 'answer to the puzzle 7',
     },
+    //  8
     {
       'path': 'images/danzaDeLaMuerte.png',
-      'name': 'Puzzle 8',
+      'name': 'Logical puzzle 8',
       'description': 'This is the description of the puzzle 8',
       'answer': 'answer to the puzzle 8',
     },
+    //  9
     {
       'path': 'images/archangelMichael.png',
-      'name': 'Puzzle 9',
+      'name': 'Logical puzzle 9',
       'description': 'This is the description of the puzzle 9',
       'answer': 'answer to the puzzle 9',
     },
@@ -75,108 +96,120 @@ class _LogicPageState extends State<LogicPage> {
   ];
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  Widget build(BuildContext context) => Container(
         // ----- Background color
-
-        backgroundColor: const Color.fromARGB(255, 100, 100, 100),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.blue,
+              // Colors.grey,
+              const Color.fromARGB(255, 50, 50, 50),
+            ],
+          ),
+        ),
+        // backgroundColor: const Color.fromARGB(255, 100, 100, 100),
 
         // ----- Body
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            fit: StackFit.expand,
 
-        body: Stack(
-          fit: StackFit.expand,
+            // ----- Add background image
+            children: [
+              //   Image.asset(
+              //     'images/skull18.png',
+              //     fit: BoxFit.cover,
+              //   ),
 
-          // ----- Add background image
-          children: [
-            Image.asset(
-              'images/skull18.png',
-              fit: BoxFit.cover,
-            ),
+              // ----- sliver app bar
 
-            // ----- Sliver app bar
-
-            CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  backgroundColor: Colors.blue,
-                  expandedHeight: 200,
-                  floating: true,
-                  pinned: true,
-                  flexibleSpace: FlexibleSpaceBar(
-                    background: Image.asset(
-                      'images/devilDrugs.png',
-                      fit: BoxFit.cover,
-                    ),
-                    title: Text(
-                      'Logic and Deduction',
-                      style: GoogleFonts.dmSerifDisplay(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontSize: 25,
+              CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    backgroundColor: Colors.transparent,
+                    expandedHeight: 200,
+                    floating: true,
+                    pinned: true,
+                    flexibleSpace: FlexibleSpaceBar(
+                      background: Image.asset(
+                        'images/devilDrugs.png',
+                        fit: BoxFit.cover,
                       ),
+                      title: Text(
+                        'Logic and Deduction',
+                        style: GoogleFonts.dmSerifDisplay(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 25,
+                        ),
+                      ),
+                      centerTitle: true,
                     ),
-                    centerTitle: true,
+                    leading: IconButton(
+                      icon: Icon(Icons.arrow_back, color: Colors.red),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
                   ),
-                  leading: IconButton(
-                    icon: Icon(Icons.arrow_back, color: Colors.red),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
 
-                // end of bar
+                  // end of bar
 
-                // sliver list
+                  // sliver list
 
-                SliverList(
-                  delegate: SliverChildListDelegate(
-                    [
-                      // promo banner
+                  SliverList(
+                    delegate: SliverChildListDelegate(
+                      [
+                        // promo banner
 
-                      Container(
-                        decoration: BoxDecoration(
-                          color: primaryColor,
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(
-                            color: Colors.orange,
-                            width: 2,
+                        Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(155, 50, 50, 50),
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: Colors.blue,
+                              width: 2,
+                            ),
+                          ),
+                          margin: const EdgeInsets.only(
+                              left: 25, right: 25, bottom: 0, top: 20),
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    // promo message
+
+                                    Text(
+                                      'Venture into the abyss:                 where logic meets lunacy.',
+                                      style: GoogleFonts.dmSerifDisplay(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 5),
+                                  ],
+                                ),
+                              ),
+
+                              // Add more content widgets here
+                            ],
                           ),
                         ),
-                        margin: const EdgeInsets.only(
-                            left: 25, right: 25, bottom: 0, top: 20),
-                        padding: const EdgeInsets.all(10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  // promo message
-
-                                  Text(
-                                    'Venture into the abyss: where logic meets lunacy.',
-                                    style: GoogleFonts.dmSerifDisplay(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.blue,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 5),
-                                ],
-                              ),
-                            ),
-
-                            // Add more content widgets here
-                          ],
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                buildImages(),
-              ],
-            ),
-          ],
+                  buildImages(),
+                ],
+              ),
+            ],
+          ),
         ),
       );
 
